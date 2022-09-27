78eae951c22fea6a26471f74b06b825905bb312e
vbojcev <vbojcev@uwaterloo.ca>
2022-09-27 17:30:48 -0400

    working GUI

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
+let rck = document.getElementById('rock');
+let ppr = document.getElementById('paper');
+let srs = document.getElementById('scissors');
+let rst = document.getElementById('reset');
+let cpuResponse = document.getElementById('computer-choice');
+let scoreboard = document.getElementById('score');
+
+let gameDict = ["rock", "paper", "scissors"];
+
+let score = [0,0];
+
+function updateScore() {
+    scoreboard.textContent = score[0]+':'+score[1];
+}
+
+rst.addEventListener('click', () => {
+    score[0] = 0;
+    score[1] = 0;
+    cpuResponse.textContent = ' ';
+    updateScore();
+});
+
 function cpuPlay() {
     return Math.floor(Math.random()*3);
 }
 
-let gameDict = ["rock", "paper", "scissors"];
+function playGame(playerEntry) {
 
-function playGame() {
+    let cpuEntry = gameDict[cpuPlay()];
 
-    for (let i=0; i<5; i++) {
-        let playerEntry = prompt("Play 'rock', 'paper', or 'scissors': ");
-        let cpuEntry = gameDict[cpuPlay()];
-        if ((cpuEntry=="rock" && playerEntry =="paper") || (cpuEntry=="paper" && playerEntry =="scissors") || (cpuEntry=="scissors" && playerEntry =="rock")) {
-            console.log("You won :) cpu played "+cpuEntry);
-        } else if (cpuEntry==playerEntry) {
-            console.log("You tied, cpu played "+cpuEntry);
-        } else if (playerEntry!="rock" && playerEntry!="paper" && playerEntry!="scissors") {
-            console.log("Please enter a valid play.");
-        } else {
-            console.log("You lost :( cpu played "+cpuEntry);
-        }
+    let result = '';
+    if ((cpuEntry=="rock" && playerEntry =="paper") || (cpuEntry=="paper" && playerEntry =="scissors") || (cpuEntry=="scissors" && playerEntry =="rock")) {
+        console.log("You won :) cpu played "+cpuEntry+" you played "+playerEntry);
+        result = 'won';
+        score[0]++;
+    } else if (cpuEntry==playerEntry) {
+        console.log("You tied, cpu played "+cpuEntry+" you played "+playerEntry);
+        result = 'tied';
+    } else if (playerEntry!="rock" && playerEntry!="paper" && playerEntry!="scissors") {
+        console.log("Please enter a valid play.");
+        result = 'did not enter a valid play';
+    } else {
+        console.log("You lost :( cpu played "+cpuEntry+" you played "+playerEntry);
+        result = 'lost';
+        score[01]++;
     }
+
+    cpuResponse.textContent = 'CPU Played '+cpuEntry+', you '+result;
+    updateScore();
 }
 
-playGame();
-console.log("Game Over!");
+rck.addEventListener('click', () => playGame(rck.id));
+ppr.addEventListener('click', () => playGame(ppr.id));
+srs.addEventListener('click', () => playGame(srs.id));
